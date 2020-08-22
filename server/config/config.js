f405bb6821efd137180632babbabbdfe8ff90543
juanmacias54 <juanmacias54@gmail.com>
2020-08-22 16:57:11 -0400

    actualizad conexion

--- server/config/config.js
+++ server/config/config.js
@@ -19,6 +19,7 @@ let urlDB;
 
 if (process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe';
+    //urlDB = 'mongodb+srv://juanmacias54:ne8Mtw123SzVCSKS@cluster0.dphbs.mongodb.net/cafe';
 }
 else{
     urlDB = process.env.MONGO_URI;
